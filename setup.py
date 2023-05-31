80a5c361e103c3c9f2404fd561072734f82b7ba3
Bathris <bathris12@gmail.com>
2023-05-31 19:03:48 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("./README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["aiogram>2"]
+
+setup(
+    name="aiogram-customizable-paginator",
+    version="0.0.1",
+    author="BathrosT",
+    author_email="bathris12@gmail.com",
+    description="Python library that provides functionality for paging through a list of objects. It allows you to easily navigate through the list using inline buttons",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BathrisT/aiogram-customizable-paginator",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[],
+)
